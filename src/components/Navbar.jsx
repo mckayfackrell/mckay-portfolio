b3979ecdc146c0dd9a9dbfdddf9455feaa720d1c
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-13 17:49:54 -0700

    updated mobile menu to appear and disappear when the hamburger is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     // navbar
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#546f7a] text-black">
@@ -21,17 +24,17 @@ const Navbar = () => {
         </ul>
 
       {/* hamburger */}
-      <div className="hidden">
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars/> : <FaTimes/>}
       </div>
 
       {/* mobile menu */}
-      <ul className="hidden">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#546f7a] flex flex-col justify-center items-center"}>
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* social icons */}
