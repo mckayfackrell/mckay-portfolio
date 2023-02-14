b8838c356e88ef27ec5c0407225346d968aa3ffd
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-13 17:41:53 -0700

    added fonts and fixed navbar menu to hide when below md pixel width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#546f7a]">
-      
-    </div>
-  )
-}
+    // navbar
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#546f7a] text-black">
+      {/* logo */}
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "60px" }} />
+      </div>
 
-export default Navbar
+      {/* menu */}
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+
+      {/* hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;
