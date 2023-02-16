f223d46fcf25e014643126485695f1ce068d668e
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-16 12:54:26 -0700

    added new text and styling to all jsx files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,25 @@ const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-black">
+            <p className="text-4xl font-bold inline border-b-4 border-black text-[#EBE9E9]">
               About
             </p>
           </div>
           <div></div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold">
-            <p>Hi, I'm McKay nice to meet you. Please take a look around.</p>
+          <div className="sm:text-right text-4xl font-bold text-[#495b67]">
+            <p>Hi, I'm McKay, nice to meet you. Please take a look around.</p>
           </div>
           <div>
-            <p>Hi, I'm McKay nice to meet you. Please take a look around.</p>
+            <p>
+              I have a strong foundation in both front-end and back-end
+              development, including HTML, CSS, JavaScript, Bootstrap, React,
+              Node.js, MERN, MySQL, and GraphQL. I am highly motivated,
+              detail-oriented, and continuously learning the latest web
+              development technologies and best practices. I am a great team
+              player with strong communication and collaboration skills.
+            </p>
           </div>
         </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,18 +6,17 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#7b904b]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#EBE9E9]">Hi, my name is</p>
+        <p className="text-[#EBE9E9] text-3xl">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-black">
           McKay Fackrell
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#35464d]">
-          I'm a Full Stack Developer
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#495b67]">
+          I'm a Full Stack Developer.
         </h2>
         <p className="py-4 max-w-[700px]">
-          Im a motivated and skilled junior full stack developer. With
-          experience in both front-end and back-end development, I am excited to
-          continue honing my skills and growing my career in the field and am
-          actively seeking new opportunities to do so.
+          I'm a motivated and skilled junior full-stack developer. With
+          experience in both front-end and back-end development, I'm ready to
+          get out there and make a difference.
         </p>
         <div>
           <button className="text-black group bg-[#5f6f3a] border-none border-2 px-6 py-3 my-2 flex items-center hover:bg-[#EBE9E9]">
